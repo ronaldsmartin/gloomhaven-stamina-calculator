811978f7f88b7bca8f7c51877dcf1788f2c32bb5
Ronald Martin <hello@itsronald.com>
2019-10-12 16:42:13 -0700

    Updates

---
+++ precache-manifest.c244eef013d6d589f326b04ddb912637.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d76050971eb968e3ff0ac5d19bf410cc",
+    "url": "/gloomhaven-stamina-calculator/index.html"
+  },
+  {
+    "revision": "c3d2e795ec83aea08dea",
+    "url": "/gloomhaven-stamina-calculator/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "4996a082e746fd992c56",
+    "url": "/gloomhaven-stamina-calculator/static/js/2.47bd2da8.chunk.js"
+  },
+  {
+    "revision": "c3d2e795ec83aea08dea",
+    "url": "/gloomhaven-stamina-calculator/static/js/main.55d212c3.chunk.js"
+  },
+  {
+    "revision": "49d2391574a19c4be5ae",
+    "url": "/gloomhaven-stamina-calculator/static/js/runtime-main.65c9a17d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/gloomhaven-stamina-calculator/static/media/logo.25bf045c.svg"
+  }
+]);
